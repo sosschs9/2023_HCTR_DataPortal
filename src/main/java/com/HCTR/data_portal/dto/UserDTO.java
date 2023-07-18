9220b1f5fc2a12e8e9b87a8df3869f6009473888
user <sosschs9@naver.com>
2023-07-18 16:55:30 +0900

    issue #4

--- src/main/java/com/HCTR/data_portal/dto/UserDTO.java
+++ src/main/java/com/HCTR/data_portal/dto/UserDTO.java
@@ -1,27 +1,36 @@
 package com.HCTR.data_portal.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import com.fasterxml.jackson.annotation.*;
 
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
 public class UserDTO {
+    @JsonProperty("Id")
     private String Id;
+    @JsonProperty("Password")
     private String Password;
-    private  String Email;
-    private String Name;
+    @JsonProperty("Email")
+    private String Email;
+    @JsonProperty("UserName")
+    private String UserName;
+    @JsonProperty("Organization")
     private String Organization;
+    @JsonProperty("Role")
     private int Role;
 
     public UserDTO(
             String id, String password, String email,
-            String name, String organization, int role){
+            String userName, String organization, int role){
         this.Id = id;
         this.Password = password;
         this.Email = email;
-        this.Name = name;
+        this.UserName = userName;
         this.Organization = organization;
         this.Role = role;
     }
